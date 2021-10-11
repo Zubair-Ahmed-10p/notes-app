902870d483bcfd3378359a8bc3356c4eab492940
zubair.ahmed <zubair.ahmed@10pearls.com>
2021-10-12 00:21:13 +0500

    section 3.12

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const chalk = require("chalk");
 const validator = require("validator");
 const { add } = require("./utils");
 const { getNotes } = require("./notes");
@@ -10,3 +11,7 @@ console.log({ sum, notes });
 console.log({ isEmail: validator.isEmail("abc@mail.com") });
 
 console.log({ isUrl: validator.isURL("abc@mail.com") });
+
+console.log(chalk.greenBright.bold.italic.bgBlack("Success :)"));
+
+console.log(chalk.redBright.bold.underline.bgBlack("Error :("));
