b250ae4bafdd679d80e93ddc2fb0eda979c1cba3
zubair.ahmed <zubair.ahmed@10pearls.com>
2021-10-12 00:07:20 +0500

    section 3.11

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const validator = require("validator");
 const { add } = require("./utils");
 const { getNotes } = require("./notes");
 
@@ -5,3 +6,7 @@ const sum = add(3, 4);
 const notes = getNotes();
 
 console.log({ sum, notes });
+
+console.log({ isEmail: validator.isEmail("abc@mail.com") });
+
+console.log({ isUrl: validator.isURL("abc@mail.com") });
