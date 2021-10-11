83267d8d3b1aab6182ad2359ab74820ba64ae955
zubair.ahmed <zubair.ahmed@10pearls.com>
2021-10-11 23:48:09 +0500

    section 3.10

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
+const { add } = require("./utils");
+const { getNotes } = require("./notes");
 
-// write sommething new
-fs.writeFileSync('notes.txt', 'My is Zubair Ahmed.');
+const sum = add(3, 4);
+const notes = getNotes();
 
-// add something to the existing content
-fs.appendFileSync('notes.txt', ' And I am a Node.js developer.'); 
+console.log({ sum, notes });
---
+++ utils.js
@@ -0,0 +1,9 @@
+console.log("utils.js");
+
+const names = "Sheikh Zubair";
+
+const add = (opr1, opr2) => {
+  return opr1 + opr2;
+};
+
+module.exports = { names, add };
