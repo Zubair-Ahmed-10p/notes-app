5ea03b74fa5cce5428b62a462b16cc31aa3914fc
zubair.ahmed <zubair.ahmed@10pearls.com>
2021-11-07 02:52:28 +0500

    section 4.17 - adding options to add command

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ yargs.version("1.0.1");
 yargs.command({
   command: "add",
   describe: "Add a new note",
-  handler: () => {
-    console.log("Adding a new note!");
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    console.log("Title: ", argv?.title);
+    console.log("Body: ", argv?.body);
   },
 });
 
@@ -42,5 +55,5 @@ yargs.command({
 });
 
 // add, remove, read, list
-
-console.log(yargs.argv);
+yargs.parse();
+// console.log(yargs.argv);
